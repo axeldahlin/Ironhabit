25d523efcf92217821133900850ccba0c8437c18
Anthony Ioimo <anthonylioimo@gmail.com>
2018-11-10 23:36:57 +0100

    fixed the formatArray function

--- routes/index.js
+++ routes/index.js
@@ -2,41 +2,35 @@ const express = require('express');
 const router  = express.Router();
 const Goal = require("../models/Goal");
 
-function formatArray(historyArr) {
-  if (historyArr.length < 42) {
-    const differens = 42 - historyArr.length;
-    for (let i = 0; i < differens; i++) {
-      historyArr.unshift(0)
-    }
-  } 
-  if (historyArr.length > 42) {
-    historyArr = historyArr.slice(historyArr.length-42)
+
+// function formatArray(historyArr) {
+//   if (historyArr.length < 42) {
+//     const differens = 42 - historyArr.length;
+//     for (let i = 0; i < differens; i++) {
+//       historyArr.unshift(0)
+//     }
+//   } 
+//   if (historyArr.length > 42) {
+//     historyArr = historyArr.slice(historyArr.length-42)
+//   }
+//     return historyArr
+// }
+
+
+function formatArray(arr,desiredLength) {
+  for (let i = 0; i<desiredLength; i++) {
+    arr.unshift(0);
   }
-    return historyArr
+  return arr.slice(arr.length-desiredLength)
 }
+
 
 /* GET home page */
-
-//with leading 0s
-function turnArraytoMatrix(arr,width,height) {
-  let totalSize = width*height;
-  newArray = arr;
-  for (let i = 0; i<totalSize-arr.length;i++) {
-    newArray.unshift(0);
-  }
-  return newArray
-
-
-}
-
-
-
 router.get('/', (req, res, next) => {
   Goal.find()
   .then(goals=> {
-    
     for (let i = 0; i<goals.length; i++) {
-      goals[i].history = formatArray(goals[i].history)
+      goals[i].history = formatArray(goals[i].history,42)
       console.log(goals[i].history)
     }
     res.render('index',{goals});
