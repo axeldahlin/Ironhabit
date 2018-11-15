4551523caaa4ecef50e3bdd08290b4aed11b37c9
Axel <axdahlin@gmail.com>
2018-11-15 09:16:47 +0100

    add design of profile page

--- public/javascripts/profile/profile.js
+++ public/javascripts/profile/profile.js
@@ -1,24 +1,12 @@
 var ctx = document.getElementById("myChart").getContext('2d');
 
+
+
+
 const ourApi = axios.create({
   baseURL: '/api/userdata'
 })
 
-<<<<<<< HEAD
-=======
-var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'pie',
-    data: {
-    datasets: [{
-        data: [10, 20, 30],
-        backgroundColor: [
-        'rgb(200, 0, 0)',
-        'yellow',
-        'blue'
-    ]
-    }],
->>>>>>> b05e9fc669f1e8617ca7fbf804421385455f9f3d
 
 ourApi.get()
   .then(res => {
