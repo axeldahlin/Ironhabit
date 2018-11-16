fb2ac176f1964a9cf1dbfb5a6c5cf23f44910037
Axel <axdahlin@gmail.com>
2018-11-16 11:01:25 +0100

    add functional bars chart

--- public/javascripts/profile/profile.js
+++ public/javascripts/profile/profile.js
@@ -1,6 +1,7 @@
 var ctxPie = document.getElementById("pie-chart").getContext('2d');
+var densityCanvas = document.getElementById("densityChart");
 
-var ctxBar = document.getElementById("bar-graph").getContext('2d');
+// var ctxBar = document.getElementById("bar-graph").getContext('2d');
 
 const ourApi = axios.create({
   baseURL: '/api/userdata'
@@ -8,16 +9,14 @@ const ourApi = axios.create({
 
 ourApi.get()
   .then(res => {
-    console.log("Response from Axios GET", res.data)
-    // var ctxPie = document.getElementById("myChart").getContext('2d');
     var pieChart = new Chart(ctxPie, {
       type: 'pie',
       data: {
         datasets: [{
             data: Object.values(res.data.successDaysTotal),
             backgroundColor: [
-            'green',
-            '#d5dbd2'
+            '#068587',
+            '#222121'
         ]
       }],
       // These labels appear in the legend and in the tooltips when hovering different arcs
@@ -35,49 +34,63 @@ ourApi.get()
     }
     });
 
-    var barGraph = new Chart(ctxBar, {
-      type: 'horizontalBar',
-      data: {
-        datasets: [{
-          data: Object.values(res.data.dailySuccessRate),
-          backgroundColor: [
-            "#068587",
-            "black",
-            "#068587",
-            "black",
-            "#068587",
-            "black"
-          ]
-        }],
-        labels: Object.keys(res.data.dailySuccessRate)
-      },
-      options: {
-        responsive: true,
-        legend: {
-          display: false,
-          labels: {
-            fontSize: 36
-          },
-        },
-        scales: {
-          yAxes: [{
-            ticks: {
-              fontSize: 36
-            }
-          }],
-          xAxes: [{
-            ticks: {
-              stepSize: 0.25,
-              fontSize: 36
-            }
-          }]
-        }
-      }
-    })
+    Chart.defaults.global.defaultFontFamily = "Lato";
+Chart.defaults.global.defaultFontSize = 18;
 
+var densityData = {
+  label: 'Consistency Percentage',
+  data: Object.values(res.data.dailySuccessRate),
+  backgroundColor: [
+    '#068587',
+    '#068587',
+    '#068587',
+    '#068587',
+    '#068587',
+    '#068587',
+    '#068587',
+    '#068587',
+    '#068587'
+  ],
+  borderColor: [
+    '#222121',
+    '#222121',
+    '#222121',
+    '#222121',
+    '#222121',
+    '#222121',
+    '#222121',
+    '#222121',
+    '#222121'
+  ],
+  borderWidth: 2,
+  hoverBorderWidth: 0
+};
+
+var chartOptions = {
+  scales: {
+    yAxes: [{
+      barPercentage: 0.5
+    }]
+  },
+  elements: {
+    rectangle: {
+      borderSkipped: 'left',
+    }
+  }
+};
+
+var barChart = new Chart(densityCanvas, {
+  type: 'horizontalBar',
+  data: {
+    labels: Object.keys(res.data.dailySuccessRate),
+    datasets: [densityData],
+  },
+  options: chartOptions
+});
+
+
+    
   })
   .catch(err=>{
     console.log("Error at axios GET", err)
   })
-
-
